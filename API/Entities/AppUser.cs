11d91fcfe576a611a06b6d9f470551656b9c9ed6
Dom Le <lequydung101@yahoo.com>
2021-06-23 03:47:53 +1000

    End of session API

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,11 @@
+namespace API.Entities
+{
+  public class AppUser
+  {
+    public int Id { get; set; } //nen dung Id de no khop voi entities
+    public string UserName { get; set; }
+
+    //public / protected / private
+    
+  }
+}
